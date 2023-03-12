ccb647266ed58c80981feac23160a40a6f8eeec9
Pavel <malinatrashh@gmail.com>
2023-03-12 16:43:39 +0800

    Добавил возможность отката на предыдущее и следующее действие

--- client/src/store/canvasState.js
+++ client/src/store/canvasState.js
@@ -1,54 +1,57 @@
-import { makeAutoObservable } from 'mobx'
+import {makeAutoObservable} from 'mobx'
 
 class CanvasState {
-	canvas = null
-	undoList = []
-	redoList = []
-	constructor() {
-		makeAutoObservable(this)
-	}
-	setCanvas(canvas) {
-		this.canvas = canvas
-	}
-	pushToUndo(data) {
-		this.undoList.push(data)
-	}
-	pushToRedo(data) {
-		this.redoList.push(data)
-	}
-	undo() {
-		let context = this.canvas.getContext('2d')
-		this.pushToRedo(context)
-		if (this.undoList.length > 0) {
-			console.log(this.undoList.length)
-			let data = this.undoList.pop()
-			this.redoList.push(data)
-			let image = new Image()
-			image.src = data
-			image.onload = () => {
-				context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-				context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height)
-			}
-		} else {
-			context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-		}
-	}
-	redo() {
-		let context = this.canvas.getContext('2d')
-		if (this.redoList.length > 0) {
-			console.log(this.redoList.length)
-			let data = this.redoList.pop()
-			this.undoList.push(this.canvas.getContext('2d'))
-			let image = new Image()
-			image.src = data
-			image.onload = () => {
-				context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-				context.drawImage(image, 0, 0, this.canvas.width, this.canvas.height)
-			}
-		} else {
-			context.clearRect(0, 0, this.canvas.width, this.canvas.height)
-		}
-	}
+    canvas = null
+    undoList = []
+    redoList = []
+
+    constructor() {
+        makeAutoObservable(this)
+    }
+
+    setCanvas(canvas) {
+        this.canvas = canvas
+    }
+
+    pushToUndo(data) {
+        this.undoList.push(data)
+    }
+
+    pushToRedo(data) {
+        this.redoList.push(data)
+    }
+
+    undo() {
+        let ctx = this.canvas.getContext('2d')
+        if (this.undoList.length > 0) {
+            let dataUrl = this.undoList.pop()
+            this.redoList.push(this.canvas.toDataURL())
+            let img = new Image()
+            img.src = dataUrl
+            img.onload = () => {
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+                ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
+            }
+        } else {
+            ctx.clearRect(0, 0, this.canvas.width, this.canvas.heigth)
+        }
+    }
+
+    redo() {
+        let ctx = this.canvas.getContext('2d')
+        if (this.redoList.length > 0) {
+            let dataUrl = this.redoList.pop()
+            this.undoList.push(this.canvas.toDataURL())
+            let img = new Image()
+            img.src = dataUrl
+            img.onload = () => {
+                ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+                ctx.drawImage(img, 0, 0, this.canvas.width, this.canvas.height)
+            }
+        }
+    }
+
 }
+
 
 export default new CanvasState()
